ec18b4a0c9c510334e2a0d15d0171ecf394a7a4d
HacksLua <parkyrist.ak@gmail.com>
2025-08-06 05:16:07 +0300

    PORT DiscordAuth DeadSpace (#105)

--- Content.Shared/Corvax/CCCVars/CCCVars.cs
+++ Content.Shared/Corvax/CCCVars/CCCVars.cs
@@ -102,7 +102,7 @@ public sealed class CCCVars
     ///     Enabled Discord linking, show linking button and modal window
     /// </summary>
     public static readonly CVarDef<bool> DiscordAuthEnabled =
-        CVarDef.Create("discord_auth.enabled", false, CVar.SERVERONLY);
+        CVarDef.Create("discord_auth.enabled", false, CVar.SERVER | CVar.REPLICATED);
 
     /// <summary>
     ///     URL of the Discord auth server API
@@ -115,4 +115,7 @@ public sealed class CCCVars
     /// </summary>
     public static readonly CVarDef<string> DiscordAuthApiKey =
         CVarDef.Create("discord_auth.api_key", "", CVar.SERVERONLY | CVar.CONFIDENTIAL);
+
+    public static readonly CVarDef<bool> DiscordAuthIsOptional =
+        CVarDef.Create("discord_auth.is_opt", false, CVar.SERVER | CVar.REPLICATED);
 }
